3cf89abde047b3c56da52bd3b9cfd9fd5753a27e
kongielowa <lidiaagatakongiel@gmail.com>
2023-04-23 11:49:48 +0200

    Update DzialPracownikow.java
    Aby umożliwić uzyskanie informacji o zadaniach, w których brał udział każdy pracownik, dodano do klasy Pracownik pole typu List<Zadanie> i metody dodające i usuwające zadania z tej listy. Dodano również metodę zwracającą listę zadań danego pracownika.
    
    Natomiast, aby każdy dział miał możliwość dowiedzenia się, jakich posiada pracowników, dodano do klasy DzialPracownikow pole typu List<Pracownik> i metody dodające i usuwające pracowników z tej listy. Dodano również metodę zwracającą listę pracowników danego działu.

--- com/GUIprojekt1/DzialPracownikow.java
+++ com/GUIprojekt1/DzialPracownikow.java
@@ -1,21 +1,26 @@
 package com.GUIprojekt1;
-
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DzialPracownikow {
     private String nazwa;
     private static List<DzialPracownikow> listaDzialow = new ArrayList<>();
+    private Map<Pracownik, List<Zadanie>> zadaniaPracownikow = new HashMap<>();
+    private List<Pracownik> pracownicy = new ArrayList<>();
 
     public DzialPracownikow(String nazwa) {
         this.nazwa = nazwa;
     }
 
     public String getNazwa() {
+
         return nazwa;
     }
 
     public void setNazwa(String nazwa) {
+
         this.nazwa = nazwa;
     }
 
@@ -31,5 +36,28 @@ public class DzialPracownikow {
 
     public static void usunDzial(DzialPracownikow dzial) {
         listaDzialow.remove(dzial);
+    }
+
+    public void dodajPracownika(Pracownik pracownik) {
+        pracownicy.add(pracownik);
+    }
+
+    public void usunPracownika(Pracownik pracownik) {
+        pracownicy.remove(pracownik);
+    }
+
+    public List<Pracownik> getPracownicy() {
+        return pracownicy;
+    }
+
+    public void dodajZadanie(Pracownik pracownik, Zadanie zadanie) {
+        if (!zadaniaPracownikow.containsKey(pracownik)) {
+            zadaniaPracownikow.put(pracownik, new ArrayList<>());
+        }
+        zadaniaPracownikow.get(pracownik).add(zadanie);
+    }
+
+    public Map<Pracownik,List<Zadanie>> getZadaniaPracownikow() {
+        return zadaniaPracownikow;
     }
 }
